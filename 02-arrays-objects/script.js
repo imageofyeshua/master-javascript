89a5261bb8cb2c430d345dbff1b9e58cfe52365d
Daniel Park <intheimageofyeshua@gmail.com>
2025-12-19 20:49:11 +0900

    Basic Array Methods

--- 02-arrays-objects/script.js
+++ 02-arrays-objects/script.js
@@ -20,6 +20,21 @@ fruits[2] = "Grape";
 fruits[fruits.length] = "Blueberry";
 fruits[fruits.length] = "Peach";
 
+numbers.pop();
+numbers.push(100);
+numbers.unshift(99);
+numbers.shift();
+numbers.reverse();
+
+x = numbers.includes(100);
+x = numbers.indexOf(33);
+
+x = numbers.slice(1, 4);
+
+// x = numbers.splice(1, 4); // change original array
+
+x = numbers.splice(1, 4).reverse().toString().charAt(0);
+
 console.log(x);
 console.log(numbers);
 console.log(fruits);
