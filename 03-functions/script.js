1f0923479a6de339b0c1a39a6a2ecf9f9b28405b
Daniel Park <intheimageofyeshua@gmail.com>
2025-12-21 17:55:03 +0900

    Scopes

--- 03-functions/script.js
+++ 03-functions/script.js
@@ -1,51 +1,74 @@
-console.log("### Params & Arguments ###");
+console.log("### Scopes ###");
 
-function registerUser(user = "Bot") {
-  // Old Syntax
-  //   if (!user) {
-  //     user = "Bot";
-  //   }
-  return user + " is registered";
+// alert("Ohh");
+// console.log(window.innerHeight);
+
+const x = 100;
+
+console.log(x, "in global");
+
+function run() {
+  console.log(window.innerHeight);
+  console.log(x, "in function");
 }
 
-console.log(registerUser("John"));
-console.log(registerUser());
+run();
 
-// Rest Params
-function sum(...numbers) {
-  let total = 0;
+if (true) {
+  const y = 200;
+  console.log(x, "in block");
+  console.log(x + y);
+}
 
-  for (const num of numbers) {
-    total += num;
+function add() {
+  const x = 1;
+  const y = 50;
+  console.log(x + y);
+}
+
+add();
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// var is not block scope
+// var varaible is added into window object
+if (true) {
+  const a = 500;
+  let b = 600;
+  var c = 700;
+}
+
+console.log(c);
+
+function run() {
+  var d = 144;
+  console.log(d);
+}
+
+run();
+
+const foo = 1;
+var bar = 2;
+
+function first() {
+  const x = 100;
+
+  function second() {
+    const y = 200;
+    console.log(x + y);
   }
-  return total;
+  second();
 }
 
-console.log(sum(1, 2, 3, 4, 5, 6, 7));
+first();
 
-// Objects as params
-function loginUser(user) {
-  return `The user ${user.name} with the id of ${user.id} is logged in`;
+if (true) {
+  const x = 123;
+
+  if (x === 123) {
+    const y = 456;
+    console.log(x + y);
+  }
 }
-
-const user = {
-  id: 12,
-  name: "John",
-};
-
-console.log(loginUser(user));
-console.log(
-  loginUser({
-    id: 7,
-    name: "Sarah",
-  })
-);
-
-// Arrays as params
-function getRandom(arr) {
-  const randomIndex = Math.floor(Math.random() * arr.length);
-  const item = arr[randomIndex];
-  console.log(item);
-}
-
-getRandom([1, 2, 3, 4, 5, 6, 7]);
---
+++ 03-functions/script_02.js
@@ -0,0 +1,51 @@
+console.log("### Params & Arguments ###");
+
+function registerUser(user = "Bot") {
+  // Old Syntax
+  //   if (!user) {
+  //     user = "Bot";
+  //   }
+  return user + " is registered";
+}
+
+console.log(registerUser("John"));
+console.log(registerUser());
+
+// Rest Params
+function sum(...numbers) {
+  let total = 0;
+
+  for (const num of numbers) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sum(1, 2, 3, 4, 5, 6, 7));
+
+// Objects as params
+function loginUser(user) {
+  return `The user ${user.name} with the id of ${user.id} is logged in`;
+}
+
+const user = {
+  id: 12,
+  name: "John",
+};
+
+console.log(loginUser(user));
+console.log(
+  loginUser({
+    id: 7,
+    name: "Sarah",
+  })
+);
+
+// Arrays as params
+function getRandom(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  const item = arr[randomIndex];
+  console.log(item);
+}
+
+getRandom([1, 2, 3, 4, 5, 6, 7]);
