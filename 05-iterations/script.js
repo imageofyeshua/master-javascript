0745d5d353ecf4142d052f51823938a74ef5528a
Daniel Park <intheimageofyeshua@gmail.com>
2025-12-25 16:08:35 +0900

    Array Reduce with Exercise

--- 05-iterations/script.js
+++ 05-iterations/script.js
@@ -1,78 +1,102 @@
-console.log("### Array Map ###");
+console.log("### Array Reduce ###");
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const doubledNumbers = numbers.map((number) => number * 2);
+const sum = numbers.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0);
 
-console.log(doubledNumbers);
+const sum2 = numbers.reduce((acc, curr) => acc + curr, 20);
 
-// Same with forEach
+// Using a for loop
+const sum3 = () => {
+  let acc = 0;
+  for (const curr of numbers) {
+    acc += curr;
+  }
+  return acc;
+};
 
-const doubledNumbers2 = [];
+console.log(sum);
+console.log(sum2);
+console.log(sum3());
 
-numbers.forEach((number) => {
-  doubledNumbers2.push(number * 2);
-});
-
-console.log(doubledNumbers2);
-
-const companies = [
-  { name: "Company One", category: "Finance", start: 1981, end: 2004 },
-  { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
-  { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
-  { name: "Company Four", category: "Finance", start: 1989, end: 2010 },
-  { name: "Company Five", category: "Technology", start: 2009, end: 2010 },
-  { name: "Company Six", category: "Retail", start: 1987, end: 2010 },
-  { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
-  { name: "Company Eight", category: "Retail", start: 1981, end: 1989 },
-  { name: "Company Nine", category: "Technology", start: 2011, end: 2023 },
+const cart = [
+  { id: 1, name: "Product 1", price: 130 },
+  { id: 2, name: "Product 2", price: 190 },
+  { id: 3, name: "Product 3", price: 370 },
 ];
 
-// Create an array of company names
-const companyNames = companies.map((company) => company.name);
+const total = cart.reduce(function (acc, product) {
+  return acc + product.price;
+}, 0);
 
-console.log(companyNames);
+// Short version
+const total2 = cart.reduce((acc, prod) => acc + prod.price, 0);
 
-// Create an array with just company and category
-const companyInfo = companies.map((company) => {
-  return {
-    name: company.name,
-    category: company.category,
-  };
+console.log(total);
+console.log(total2);
+
+const people = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@gmail.com",
+    phone: "111-111-1111",
+    age: 30,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Poe",
+    email: "jane@gmail.com",
+    phone: "222-222-2222",
+    age: 25,
+  },
+  {
+    firstName: "Bob",
+    lastName: "Foe",
+    email: "bob@gmail.com",
+    phone: "333-333-3333",
+    age: 45,
+  },
+  {
+    firstName: "Sara",
+    lastName: "Soe",
+    email: "Sara@gmail.com",
+    phone: "444-444-4444",
+    age: 19,
+  },
+  {
+    firstName: "Jose",
+    lastName: "Koe",
+    email: "jose@gmail.com",
+    phone: "555-555-5555",
+    age: 23,
+  },
+];
+
+const youngPeople = people
+  .filter((person) => person.age <= 25)
+  .map((person) => ({
+    name: person.firstName + " " + person.lastName,
+    email: person.email,
+  }));
+
+console.log(youngPeople);
+
+const nums = [2, -30, 50, 20, -12, -9, 7];
+
+// Add all of the positive numbers in the array
+const positiveTotal = nums
+  .filter((number) => number > 0)
+  .reduce((acc, curr) => acc + curr, 0);
+
+console.log(positiveTotal);
+
+const words = ["coder", "programmer", "developer"];
+
+const capitalized = words.map((word) => {
+  return word[0].toUpperCase() + word.slice(1, word.length);
 });
 
-console.log(companyInfo);
-
-// Create an array of objects with the name and the length of each company in years
-const companyYears = companies.map((company) => {
-  return {
-    name: company.name,
-    length: company.end - company.start,
-  };
-});
-
-console.log(companyYears);
-
-// Chain map methods
-const squareAndDouble = numbers
-  .map((number) => Math.sqrt(number))
-  .map((sqrt) => sqrt * 2);
-
-const squareAndDouble2 = numbers
-  .map(function (number) {
-    return Math.sqrt(number);
-  })
-  .map(function (sqrt) {
-    return sqrt * 2;
-  })
-  .map(function (sqrtDoubled) {
-    return sqrtDoubled * 3;
-  });
-
-console.log(squareAndDouble);
-console.log(squareAndDouble2);
-
-// Chaining different methods
-const evenDouble = numbers
-  .filter((number) => number % 2 === 0)
-  .map((number) => number * 2);
+console.log(capitalized);
