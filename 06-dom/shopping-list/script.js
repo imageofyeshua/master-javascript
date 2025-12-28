88b96050b92f463144b38fc0d53e09ce01c74131
Daniel Park <intheimageofyeshua@gmail.com>
2025-12-28 13:14:33 +0900

    Create and Append Elements

--- 06-dom/shopping-list/script.js
+++ 06-dom/shopping-list/script.js
@@ -1,1 +1,15 @@
-console.log("### Traversing The DOM - Elements ###");
+console.log("### Create & Append Elements ###");
+
+const div = document.createElement("div");
+div.className = "my-element";
+div.id = "my-element";
+div.setAttribute("title", "My Element");
+
+// div.innerText = "Hello World";
+
+const text = document.createTextNode("Hello World");
+div.appendChild(text);
+
+// document.body.appendChild(div);
+
+document.querySelector("ul").appendChild(div);
