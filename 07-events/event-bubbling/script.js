fd96a3fa56fb22a5d5cd08182409b3a33aaf6d0b
Daniel Park <intheimageofyeshua@gmail.com>
2026-01-04 21:34:12 +0900

    Event Delegation

--- 07-events/event-bubbling/script.js
+++ 07-events/event-bubbling/script.js
@@ -1,21 +1,43 @@
-const button = document.querySelector("form button");
-const div = document.querySelector("form div:nth-child(2)");
-const form = document.querySelector("form");
-const body = document.querySelector("body");
+// const button = document.querySelector("form button");
+// const div = document.querySelector("form div:nth-child(2)");
+// const form = document.querySelector("form");
+// const body = document.querySelector("body");
 
-button.addEventListener("click", (e) => {
-  alert("Button was clicked");
-  e.stopPropagation();
+// button.addEventListener("click", (e) => {
+//   alert("Button was clicked");
+//   e.stopPropagation();
+// });
+
+// div.addEventListener("click", () => {
+//   alert("Div was clicked");
+// });
+
+// form.addEventListener("click", () => {
+//   alert("Form was clicked");
+// });
+
+// body.addEventListener("click", () => {
+//   alert("Body was clicked");
+// });
+
+// Event Delegation
+const listItems = document.querySelectorAll("li");
+const list = document.querySelector("ul");
+
+// listItems.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     e.target.remove();
+//   });
+// });
+
+list.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.remove();
+  }
 });
 
-div.addEventListener("click", () => {
-  alert("Div was clicked");
-});
-
-form.addEventListener("click", () => {
-  alert("Form was clicked");
-});
-
-body.addEventListener("click", () => {
-  alert("Body was clicked");
+list.addEventListener("mouseover", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.style.color = "red";
+  }
 });
