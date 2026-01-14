dd69e0df36cc7fd4d150c81e389ee298b6c761c6
Daniel Park <intheimageofyeshua@gmail.com>
2026-01-15 00:19:02 +0900

    Ajax & XHR

---
+++ 09-asynchronus/ajax-xhr/script.js
@@ -0,0 +1,28 @@
+const xhr = new XMLHttpRequest();
+
+// xhr.open("GET", "./movies.json");
+xhr.open("GET", "https://api.github.com/users/imageofyeshua/repos");
+
+// readyState has 5 possilbe values
+// - 0 : request not initialized
+// - 1 : server connection established
+// - 2 : request received
+// - 3 : processing request
+// - 4 : request finished and response is ready
+xhr.onreadystatechange = function () {
+  //   console.log(this.readyState);
+  //   console.log(this.status);
+  if (this.readyState === 4 && this.status === 200) {
+    // console.log(JSON.parse(this.responseText));
+    const data = JSON.parse(this.responseText);
+
+    console.log(data);
+    data.forEach((repo) => {
+      const li = document.createElement("li");
+      li.innerHTML = `<strong>${repo.name}</strong> - ${repo.url}`;
+      document.querySelector("#results").appendChild(li);
+    });
+  }
+};
+
+xhr.send();
